4dd57876687687a83840c492923a2f80453c01ca
BillOzmov <145689675+BillOzmov@users.noreply.github.com>
2023-11-10 23:16:15 +0800

    对接口中查询方法的完善
    方法的功能请见注释。

--- backend/src/main/java/edu/fzu/se/backend/mapper/FavoritesMapper.java
+++ backend/src/main/java/edu/fzu/se/backend/mapper/FavoritesMapper.java
@@ -12,7 +12,16 @@ import java.util.List;
 public interface FavoritesMapper {
     @Select("SELECT * FROM Favorites")
     List<Favorites> selectAll();
-
+//  selectById：根据收藏id进行查询。
+    @Select("SELECT * FROM Favorites WHERE Favorite_ID=#{favoriteId}")
+    Favorites selectById(Long favoriteId);
+//  selectByUserId：根据用户id进行查询。
+    @Select("SELECT * FROM Favorites WHERE User_ID=#{userId}")
+    List<Favorites> selectByUserId(Long userId);
+//  selectByKeyword：根据收藏商品的关键字进行模糊匹配查询。该方法使用了子查询和LIKE函数来实现。
+    @Select("SELECT * FROM Favorites WHERE Good_ID IN " +
+            "(SELECT Good_ID FROM Goods WHERE Goods_Name LIKE CONCAT('%', #{keyword}, '%'))")
+    List<Favorites> selectByKeyword(String keyword);
     @Delete("DELETE FROM Favorites WHERE Favorite_ID=#{favoriteId}")
     int deleteById(Long favoriteId);
 
