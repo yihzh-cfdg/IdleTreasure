76a1b8bc6872707f4a9e503b8f8d9c7ee74a6497
BillOzmov <145689675+BillOzmov@users.noreply.github.com>
2023-11-10 18:46:39 +0800

    对bean中的实体类添加了有参构造和无参构造两个注解

--- backend/src/main/java/edu/fzu/se/backend/bean/Evaluations.java
+++ backend/src/main/java/edu/fzu/se/backend/bean/Evaluations.java
@@ -4,11 +4,15 @@ package edu.fzu.se.backend.bean;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @TableName("Evaluations")
 public class Evaluations {
     @TableId(type = IdType.AUTO)
