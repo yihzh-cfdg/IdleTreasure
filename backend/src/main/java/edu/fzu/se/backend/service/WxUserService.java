d8b584782e2ec8671a6c59373a35ec1f08e04e75
why <why@123.com>
2023-11-11 14:12:59 +0800

    登录注册更新

--- backend/src/main/java/edu/fzu/se/backend/service/WxUserService.java
+++ backend/src/main/java/edu/fzu/se/backend/service/WxUserService.java
@@ -1,5 +1,15 @@
 package edu.fzu.se.backend.service;
-//整合后加入mapper等的引用
-public interface WxUserService extends IService<WxUser>
-    }
+
+import edu.fzu.se.backend.bean.WxUser;
+
+import java.util.List;
+
+public interface WxUserService {
+    //登录
+    boolean loginService(String name,String password);
+    //注册
+    boolean registerService(WxUser wxUser);
+    //用户名是否存在
+    boolean isUsernameExists(String name);
+}
 
