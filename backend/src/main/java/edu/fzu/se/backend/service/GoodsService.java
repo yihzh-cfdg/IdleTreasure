5738a23c9bf69cf4a8c4ce99470a02e4e3b957dc
why <why@123.com>
2023-11-13 13:06:03 +0800

    登陆注册返回主页和分类商品

--- backend/src/main/java/edu/fzu/se/backend/service/GoodsService.java
+++ backend/src/main/java/edu/fzu/se/backend/service/GoodsService.java
@@ -1,4 +1,12 @@
 package edu.fzu.se.backend.service;
 
+import edu.fzu.se.backend.bean.Goods;
+
+import java.util.List;
+
 public interface GoodsService {
+    //返回指定页面商品信息
+    List<Goods> classgetgoods (String classification);
+    //返回主页商品信息
+    public List<Goods> maingetgoods();
 }
