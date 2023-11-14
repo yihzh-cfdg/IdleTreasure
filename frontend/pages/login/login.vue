d8047b391db970ffc2e770d60eda919bbd480234
annylayx <3080561097@qq.com>
2023-11-14 20:20:39 +0800

    增加初始界面、登录、消息列表、个人中心、订单详情界面

---
+++ frontend/pages/login/login.vue
@@ -0,0 +1,160 @@
+<template>
+	<view class="content">
+		<view class="avatorWrapper">
+			<view class="avator">
+				<!-- 图标 -->
+				<image class="img" src="../../static/cat.png" mode="aspectFit"></image>
+			</view>
+		</view>
+		
+		<view class="form">
+			<uni-forms  ref="form" :modelValue="formData" :rules="rules">
+				<uni-forms-item name="iphone">
+					<input class="input" type="text" v-model="formData.iphone" placeholder="请输入用户名/手机号" />
+					
+				</uni-forms-item>
+				
+				<uni-forms-item name="password"  >
+					<input  class="input" type="password"  v-model="formData.password" placeholder="请输入密码" />
+				</uni-forms-item>
+			</uni-forms>
+		
+			<view class="loginBtn" @click="submit">
+				<text class="btnValue" style="color: black;">登录</text>
+			</view>
+		</view>
+		
+		<view class="inputArea">
+			<!-- 跳转注册界面 -->
+			<text style="float:right;color: azure;" @click=" ">新用户注册</text>
+		</view>
+		
+	</view>
+	</view>
+</template>
+
+
+
+<script>
+	export default {
+		data() {
+			return {
+							
+					formData: {
+						name:'',
+						iphone: '',
+						password:'',
+						
+						
+				},
+				rules: {   
+					
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+			submit() {
+			          //实现提交数据
+			       },
+						
+			
+			
+			login(e) {
+	
+				
+			},
+
+
+
+
+		}
+	}
+</script>
+
+
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+		background-color: #9cebff;
+	}
+
+	.avatorWrapper {
+		margin-top: 16px;
+		height: 30vh;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+	}
+
+	.avator {
+		width: 300upx;
+		height: 300upx;
+		overflow: hidden;
+	}
+
+	.avator .img {
+		width: 100%
+	}
+
+	.form {
+		padding: 0 100upx;
+		margin-top: 40px;
+	}
+
+	.input {
+		flex: 1;
+		/* margin-top: 20px; */
+		font-size: 16px;
+		width: 100%;
+		height: 70upx;
+		color: #666;
+		border: 1px #e5e5e5 solid;
+		border-radius:50px;
+		padding: 10px;
+		background-color: #ffffff;
+		/* height: 300upx; */
+		
+	}
+	.psd{
+		margin-top: 8px;
+		
+	
+	}
+	
+	.loginBtn {
+		flex:1;
+		width: 108%;
+		height: 100upx;
+		background: #5bc3e3;
+		border-radius: 50upx;
+		margin-top: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	}
+	.icon{
+		/* margin-top: 200px; */
+		margin-left: 240px;
+		margin-top: -20px;
+		/* margin-top: 10px; */
+		/* background: rgba(8, 8, 10, 0.96); */
+
+	}
+	.inputArea {
+		padding: 20upx 10%;
+		
+	}
+	.img{
+		height: 120px;
+		width: 120px;
+	}
+	
+</style>
---
+++ frontend/pages/region/region.vue
@@ -0,0 +1,134 @@
+<template>
+	<view class="content">
+		
+				<!-- 图片布局 -->
+				<image class="img1" src="../../static/p2.png" style="height: 50px;" mode="aspectFit"></image>
+				<image class="img2" src="../../static/p1.png" mode="aspectFit"></image>
+				<image class="img4" src="../../static/p3.png" style="height: 190px;" mode="aspectFit"></image>
+				<image class="img3" src="../../static/people.png" style="height: 300px;" mode="aspectFit"></image>
+			
+		
+		
+		<view class="Btn">
+			
+			<!-- 跳转登录 -->
+			<view class="loginBtn1" @click="submit">
+				<text class="btnValue" style="color: black;">登录</text>
+				
+			</view>
+			<!-- 跳转注册 -->
+			<view class="loginBtn2" @click="register">
+				<text class="btnValue" style="color: black;">注册</text>
+				
+
+			</view>
+		</view>
+		
+		
+		
+	</view>
+	</view>
+</template>
+
+
+
+<script>
+	export default {
+		data() {
+			return {	
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+			
+			submit() {
+					uni.navigateTo({
+					    url: '../login/login',
+					});
+			          //实现跳转登录
+			       },
+			
+			
+			login() {
+				uni.navigateTo({
+				    url: '  ',
+				});
+				//实现跳转注册
+			}
+		}
+	}
+</script>
+
+
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+		background-color: #9cebff;
+	}
+	.img1{
+		left: 10px;
+		top: 100.45px;
+		width: 126.78px;
+		height: 61.81px;
+		opacity: 1;
+		/* transform: rotate(0.93deg); */
+		
+	}
+	.img2{
+		left:-113px ;
+		top: 123px;
+		width: 222px;
+		height: 35px;
+		opacity: 1;
+		
+	}
+	.img3{
+		top: -50px;
+		left: 30px;
+		
+		/* width: 537px; */
+		justify-content: center;
+		align-items: flex-end;
+		
+	}
+	.img4{
+		left:147px;
+		top: -30px;
+	}
+
+	.Btn {
+		padding: 0 100upx;
+		margin-top:-50px;
+		
+	}
+
+	.loginBtn1 {
+		flex:1;
+		width: 108%;
+		height: 100upx;
+		background: #5bc3e3;
+		border-radius: 50upx;
+		margin-top: 20px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	}
+	.loginBtn2{
+		flex:1;
+		width: 108%;
+		height: 100upx;
+		background: #e8e8e8;
+		border-radius: 50upx;
+		margin-top: 20px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
+	}
+</style>
