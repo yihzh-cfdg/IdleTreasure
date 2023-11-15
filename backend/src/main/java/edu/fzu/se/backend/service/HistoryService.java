1168222547ff0efbf42736fb0a60210e1d5c6145
why <why@123.com>
2023-11-15 15:09:04 +0800

    service更新

--- backend/src/main/java/edu/fzu/se/backend/service/HistoryService.java
+++ backend/src/main/java/edu/fzu/se/backend/service/HistoryService.java
@@ -1,4 +1,22 @@
 package edu.fzu.se.backend.service;
 
+import edu.fzu.se.backend.bean.History;
+
+import java.util.List;
+
 public interface HistoryService {
+    //获取所有搜索历史记录
+    List<History> getAllHistories();
+    //根据用户ID查询全部搜索历史记录
+    List<History> getHistoriesByUserId(Long userId);
+    //根据用户ID查询最新的十条搜索历史记录
+    List<History> getLatestTenHistoriesByUserId(Long userId);
+    //根据搜索ID查询搜索历史记录
+    History getHistoryById(Long searchId);
+    //根据搜索ID删除搜索历史记录
+    int deleteHistoryById(Long searchId);
+    //新增搜索历史记录
+    int insertHistory(History history);
+    //根据搜索ID更新搜索历史记录
+    int updateHistoryById(History history);
 }
---
+++ backend/src/main/java/edu/fzu/se/backend/service/serviceimpl/ImagesServiceimpl.java
@@ -0,0 +1,4 @@
+package edu.fzu.se.backend.service.serviceimpl;
+
+public class ImagesServiceimpl {
+}
